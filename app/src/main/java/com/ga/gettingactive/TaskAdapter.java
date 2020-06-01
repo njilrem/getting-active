df52267517f70092016f12f68bffe686f00aa75f
Mykhailo Romanenko <mihrom2001@gmail.com>
2020-06-01 20:56:32 +0300

    Basic RecyclerView for task displaying created.

---
+++ app/src/main/java/com/ga/gettingactive/TaskAdapter.java
@@ -0,0 +1,69 @@
+package com.ga.gettingactive;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    private List<Task> tasksList = new ArrayList<>();
+
+    public void setItems(Collection<Task> tasks) {
+        tasksList.addAll(tasks);
+        notifyDataSetChanged();
+    }
+
+    public void clearItems() {
+        tasksList.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_item, parent, false);
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        holder.bind(tasksList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasksList.size();
+    }
+
+    class TaskViewHolder extends RecyclerView.ViewHolder{
+        private TextView titleView;
+        private TextView descriptionView;
+
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleView = itemView.findViewById(R.id.task_title);
+            descriptionView = itemView.findViewById(R.id.task_description);
+        }
+
+        public void bind(Task task){
+        titleView.setText(task.getTitle());
+        descriptionView.setText(task.getDescription());
+        }
+    }
+
+
+
+
+
+
+}
+
+
