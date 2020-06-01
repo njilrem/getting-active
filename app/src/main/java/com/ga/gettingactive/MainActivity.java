4b16079588cd3c593365f64572437145a419fa2c
njilrem <antonkucher00@gmail.com>
2020-06-01 18:02:10 +0300

    gradle version

--- app/src/main/java/com/ga/gettingactive/MainActivity.java
+++ app/src/main/java/com/ga/gettingactive/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ga.gettingactive;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        Log.d("Main Activity", "Getting Active");
     }
 
 }
