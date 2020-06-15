a464b10598fd4e958ebf7b6604784dfacea5e00d
njilrem <antonkucher00@gmail.com>
2020-06-15 14:50:36 +0300

    added fetch from db

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,10 +5,6 @@
 app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
 h
 8app/src/main/java/com/ga/gettingactive/MainActivity.java,d\5\d514faad0e76f5f8d71acd78e37f092a5e59087d
-g
-7app/src/main/java/com/ga/gettingactive/TaskAdapter.java,3\c\3cf006aff06dcd3c09688b39570e152c5916b41e
-i
-9app/src/main/java/com/ga/gettingactive/TaskContainer.java,3\a\3a3bcdf98406e89c523f178fb9fed5f0ed7dbf58
 <
 Diagram.puml,8\d\8dd405e6b6e5b47f32061366b37dbbf7fad392b3
 S
--- app/src/main/java/com/ga/gettingactive/pref/PreferencesActivity.java
+++ app/src/main/java/com/ga/gettingactive/pref/PreferencesActivity.java
@@ -1,18 +1,38 @@
 package com.ga.gettingactive.pref;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.ga.gettingactive.FirestoreDB;
 import com.ga.gettingactive.R;
+import com.ga.gettingactive.tasklist.TaskContainer;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class PreferencesActivity extends AppCompatActivity {
     private static final String[] categories = {"health", "beauty", "socialization"};
     private RecyclerView prefsView;
+    private String Tag = "Preferable tasks";
+
+    FirebaseFirestore db = FirestoreDB.db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,27 +40,77 @@ public class PreferencesActivity extends AppCompatActivity {
         setupRecyclerView();
     }
 
-    private void setupRecyclerView(){
+    private void setupRecyclerView() {
         prefsView = findViewById(R.id.prefs_recycler_view);
         prefsView.setLayoutManager(new CustomGridLayout(this, 2));
-        ArrayList<String> categoryNames = fetchNamesFromDB();
-        ArrayList<Integer> selectedCategories = fetchSelectedCategoriesFromDB();
+        ArrayList<String> categoryNames = new ArrayList<>();
+        fetchNamesFromDB(new CategoriesCallback<String>() {
+            @Override
+            public void onCallback(List<String> list) {
+                categoryNames.addAll(list);
+            }
+        });
+        Log.d(Tag, String.valueOf(categoryNames));
+        ArrayList<Integer> selectedCategories = new ArrayList<>();
+        fetchSelectedCategoriesFromDB(new CategoriesCallback<Integer>() {
+            @Override
+            public void onCallback(List<Integer> list) {
+                selectedCategories.addAll(list);
+            }
+        });
+        Log.d(Tag, String.valueOf(selectedCategories));
         PrefListAdapter adapter = new PrefListAdapter(categoryNames, selectedCategories);
         prefsView.setAdapter(adapter);
     }
 
-    private ArrayList<Integer> fetchSelectedCategoriesFromDB() {
-        //for toha
+    private void fetchSelectedCategoriesFromDB(CategoriesCallback<Integer> callback) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        return list;
+        if (user != null) {
+            DocumentReference userProfile = db.collection("users").document(user.getUid());
+            userProfile.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Object categories = document.get("categories");
+                        if (categories == null) {
+                            HashMap<String, Object> field = new HashMap<>();
+                            field.put("categories", list);
+                            userProfile.update(field);
+                        } else {
+                            Log.d(Tag, "fetch selected " + categories);
+                            callback.onCallback((ArrayList<Integer>)categories);
+                        }
+                    } else {
+                        Log.d(Tag, "No such document");
+                    }
+                } else {
+                    Log.d(Tag, "get failed with ", task.getException());
+                }
+            });
+        }
     }
 
-    private ArrayList<String> fetchNamesFromDB() {
-        //for toha
-        ArrayList<String> arr = new ArrayList<>();
-        for(String s : categories)
-            arr.add(s);
-        return arr;
+    private void fetchNamesFromDB(CategoriesCallback<String> callback) {
+        db.collection("categories").document("categories").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    Object categories = document.get("categories");
+                    ArrayList<String> res = (ArrayList<String>) categories;
+                    Log.d("Categories", String.valueOf(res));
+                    callback.onCallback(res);
+                } else {
+                    Log.d(Tag, "No such document");
+                }
+            } else {
+                Log.d(Tag, "get failed with ", task.getException());
+            }
+        });
+    }
+
+    private interface CategoriesCallback<T>{
+        void onCallback(List<T> list);
     }
 }
+
